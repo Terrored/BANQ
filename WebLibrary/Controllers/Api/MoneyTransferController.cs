4e891ece2c9e419e46b4cec7512aa8a299fd5a75
Krzysztof Milewski <milewski123@onet.pl>
2019-03-05 16:22:50 +0100

    Introducing Web API controllers - WIP

---
+++ WebLibrary/Controllers/Api/MoneyTransferController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLogic.Interfaces;
+using System.Web.Http;
+using WebLibrary.IdentityExtensions;
+
+namespace WebLibrary.Controllers.Api
+{
+    public class MoneyTransferController : ApiController
+    {
+        private readonly IMoneyTransferService _moneyTransferService;
+
+        public MoneyTransferController(IMoneyTransferService moneyTransferService)
+        {
+            _moneyTransferService = moneyTransferService;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetLastFive()
+        {
+            var userId = User.Identity.GetUserId().Value;
+            var transfers = _moneyTransferService.GetLastSentFiveTransfers(userId);
+            return Ok(transfers);
+        }
+    }
+}
