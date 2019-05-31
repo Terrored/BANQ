532baa7db384bd2e590a03223a9135314d8a847f
Krzysztof Milewski <milewski123@onet.pl>
2019-05-31 18:05:02 +0200

    added data attributes to scripts tag for passing arguments

---
+++ WebLibrary/Scripts/app/moneyTransfer/index.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(function () {
+
+    var scriptTag = $("#indexMoneyTransferScript");
+    var userId = scriptTag.data("userid");
+    $('#lastTransfers').DataTable({
+        "ajax": { url: "/api/MoneyTransfer/GetLastFive", dataSrc: "" },
+        "paging": false,
+        "ordering": false,
+        "info": false,
+        "searching": false,
+        "columns": [
+            { "data": "name" },
+            { "data": "id" },
+            { "data": "from.userName" },
+            { "data": "to.userName" },
+            {
+                "data": "cashAmount", "render": function (data, type, row, meta) {
+                    if (row.from.id == userId) {
+                        data = -data;
+                    }
+
+                    return data.toFixed(2).toLocaleString() + " PLN";
+                }
+            },
+            {
+                "data": "createdOn", "render": function (data, type, row, meta) {
+
+                    return data.substring(0, 10);
+                }
+            }
+        ],
+        "rowCallback": function (row, data, index) {
+
+            if (data.from.id === userId) {
+                $('td', row).css('color', '#ff3232');
+                $('td', row).css('font-weight', '500');
+            } else {
+                $('td', row).css('color', '#50C878');
+                $('td', row).css('font-weight', '500');
+            }
+        }
+    });
+});
---
+++ WebLibrary/Scripts/app/moneyTransfer/transfers.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(function () {
+    var scriptTag = $("#transfersScript");
+    var userId = scriptTag.data("userid");
+
+    $('#allTransfers').DataTable({
+        "ajax": { url: "/api/MoneyTransfer/GetAllTransfers", dataSrc: "" },
+        "paging": false,
+        "order": [[5, "desc"]],
+        "info": false,
+        "searching": false,
+        "columns": [
+            { "data": "name" },
+            { "data": "id" },
+            { "data": "from.userName" },
+            { "data": "to.userName" },
+            {
+                "data": "cashAmount", "render": function (data, type, row, meta) {
+                    if (row.from.id == userId) {
+                        data = -data;
+                    }
+
+                    return data.toFixed(2).toLocaleString() + " PLN";
+                }
+            },
+            {
+                "data": "createdOn", "render": function (data, type, row, meta) {
+
+                    return data.substring(0, 10);
+                }
+            }
+        ],
+        "rowCallback": function (row, data, index) {
+
+            if (data.from.id === userId) {
+                $('td', row).css('color', '#ff3232');
+                $('td', row).css('font-weight', '500');
+            } else {
+                $('td', row).css('color', '#50C878');
+                $('td', row).css('font-weight', '500');
+            }
+        }
+    });
+});
